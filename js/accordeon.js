8bfbcf6350ae23675fc3b3884c689277227718b6
VetalSh <jedvemya86@gmail.com>
2022-04-23 22:18:26 +0300

    js2

--- js/accordeon.js
+++ js/accordeon.js
@@ -5,10 +5,33 @@ contents.forEach((elem) => {
   const title = elem.querySelector(".program-line__title");
   const descr = elem.querySelector(".program-line__descr");
 
+  descr.style.transition = "height .3s";
+  descr.style.overflow = "hidden";
+
   title.addEventListener("click", () => {
     descrs.forEach((item) => {
-      item.classList.remove("active");
+      if (item === descr) {
+        item.style.height = item.scrollHeight + "px";
+      } else {
+        item.style.height = 0;
+      }
     });
-    descr.classList.toggle("active");
   });
+
+  // title.addEventListener("click", () => {
+  //   descrs.forEach((item) => {
+  //     if (item === descr) {
+  //       item.classList.add("active");
+  //     } else {
+  //       item.classList.remove("active");
+  //     }
+  //   });
+  // });
+
+  // title.addEventListener("click", () => {
+  //   descrs.forEach((item) => {
+  //     item.classList.remove("active");
+  //   });
+  //   descr.classList.toggle("active");
+  // });
 });
